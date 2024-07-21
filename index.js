5457d8340b05fbba8cac1897cfdd79f0a5d5d691
Lola Pushkar <112199036+olhaluhova@users.noreply.github.com>
2024-07-21 14:25:29 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+import express from 'express';
+const app = express();
+
+app.get('/', (req, res) => {
+  const name = process.env.NAME || 'Google Cloud Serverless community';
+  res.send(`Hello ${name}! This is built using the code from my GitHub repo.`);
+});
+
+const port = parseInt(process.env.PORT) || 8080;
+app.listen(port, () => {
+  console.log(`helloapp: listening on port ${port}`);
+});
